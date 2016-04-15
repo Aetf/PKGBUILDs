685f47251447bfe3d2b4d5c9a825d435671a821c
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-15 15:24:27 -0400

    linux: update to 4.5-1

--- linux/changeset/apply.sh
+++ linux/changeset/apply.sh
@@ -2,6 +2,15 @@
 SCRIPT=$(readlink -f "$0")
 DIRPATH=$(dirname "$SCRIPT")
 
+cd $DIRPATH
+yaourt -G linux
+
+cd $DIRPATH/..
+find  . -maxdepth 1 -depth ! -name 'changeset' -and ! -name '.gitignore' -delete
+
+mv $DIRPATH/linux/* $DIRPATH/..
+rmdir $DIRPATH/linux
+
 patch -p0 -i "$DIRPATH/PKGBUILD.diff"
 for f in $DIRPATH/*.patch
 do
@@ -9,3 +18,4 @@ do
     echo "Adding $name"
     ln -rs "$f" "$name"
 done
+updpkgsums
